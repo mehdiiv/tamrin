97db1d0e2e2e2e15343bb2ec6f9d2d9e65a9c0b6
Mohsen <mo.ahmadzade@gmail.com>
2024-10-22 17:36:30 +0200

    fix tests

--- room/test/test_url.py
+++ room/test/test_url.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from room.views import (
-    RoomDetailView, RoomListView,
+    RoomDetailView, RoomsListView,
    RoomCreateView,
 )
 from django.urls import resolve, reverse
@@ -8,15 +8,12 @@ from django.urls import resolve, reverse
 class UrlTest(SimpleTestCase):
     def test_room_list_url(self):
         url = reverse('rooms_list', kwargs={'pk': 1 })
-        self.assertEqual(resolve(url).func.view_class, RoomListView)
-    
+        self.assertEqual(resolve(url).func.view_class, RoomsListView)
+
     def test_room_create_url(self):
         url = reverse('room_create',kwargs={'pk': 1 })
         self.assertEqual(resolve(url).func.view_class, RoomCreateView)
-    
+
     def test_room_detail_url(self):
         url = reverse('room_detail',kwargs={'pk': 1,'room_pk' : 1 })
         self.assertEqual(resolve(url).func.view_class, RoomDetailView)
-    
-
-
--- room/test/test_views.py
+++ room/test/test_views.py
@@ -5,38 +5,36 @@ from django.contrib.auth.models import User
 from room.views import Room
 
 class RoomViewTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='test', password='1223234')
-        self.client = Client()
-        self.client.login(username='test', password='1223234')
-        self.building = Building.objects.create(creator = self.user, built_year = '1995-12-12',room_number = '25',area = '123',floors = '3',Colour = 'yellow')
-        self.room = Room.objects.create(building = self.building, area = '12', capacity = '53', room_name = 'testroom' )
+  def setUp(self):
+    self.user = User.objects.create_user(username='test', password='1223234')
+    self.client = Client()
+    self.client.login(username='test', password='1223234')
+    self.building = Building.objects.create(creator = self.user, built_year = '1995-12-12',room_number = '25',area = '123',floors = '3',Colour = 'yellow')
+    self.room = Room.objects.create(building = self.building, area = '12', capacity = '53', room_name = 'testroom' )
 
 
-"""     def test__list_view(self):
-        response = self.client.get(reverse('rooms_list'), kwargs={ 'pk': self.building.id})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'room/list.html')
-        self.assertContains(response, self.room.building)
-    
-    def test_room_list_view_logout_auth_user(self):
-        logout_client = Client()
-        response = logout_client.get(reverse('rooms_list'), kwargs={ 'pk': self.building.id})
-        self.assertEqual(response.status_code, 302)
-    
-    def test_room_detail_view(self):
-        response = self.client.get(reverse('room_create', kwargs={ 'pk': self.building.id ,'room_pk' : self.room.id }))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'room/detail.html')
-        self.assertContains(response, self.building.creator)
-    
-    def test_room_detail_view_logout_auth_user(self):
-        logout_client = Client()
-        response = logout_client.get(reverse('building_detail', kwargs={ 'pk': self.building.id ,'room_pk' : self.room.id }))
-        self.assertEqual(response.status_code, 302)
-    
-    def test_room_create_view(self):
-        response = self.client.post(reverse('room_create'),{ 'area' : '12' ,'capacity' : '53','room_name' : 'testroom',}, kwargs={ 'pk': self.building.id })
-        self.assertEqual(response.status_code, 302)
-        self.assertTrue(Building.objects.filter(area = '12').exists())
-     """
+  def test__list_view(self):
+    response = self.client.get(reverse('rooms_list', kwargs={ 'pk': self.building.id }))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'room/list.html')
+    self.assertContains(response, self.room.building)
+
+  def test_room_list_view_logout_auth_user(self):
+    logout_client = Client()
+    response = logout_client.get(reverse('rooms_list', kwargs={ 'pk': self.building.id}))
+    self.assertEqual(response.status_code, 302)
+
+  def test_room_detail_view(self):
+    response = self.client.get(reverse('room_create', kwargs= { 'pk': self.building.id }))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'room/form.html')
+    self.assertContains(response, self.building.creator)
+
+  def test_room_detail_view_logout_auth_user(self):
+    logout_client = Client()
+    response = logout_client.get(reverse('room_detail', kwargs={ 'pk': self.building.id ,'room_pk' : self.room.id }))
+    self.assertEqual(response.status_code, 302)
+
+  def test_room_create_view(self):
+    response = self.client.post(reverse('room_create', kwargs= { 'pk': self.building.id }), { 'area' : '12' ,'capacity' : '53','room_name' : 'testroom'})
+    self.assertEqual(response.status_code, 302)
